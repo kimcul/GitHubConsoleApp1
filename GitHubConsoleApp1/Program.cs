f84eb67eade340cb2b6a72826d57aedaa439bb4b
kimcul <culpritkjb@gmail.com>
2023-09-15 12:56:57 +0900

    커밋2

--- GitHubConsoleApp1/Program.cs
+++ GitHubConsoleApp1/Program.cs
@@ -16,7 +16,7 @@ namespace GitHubConsoleApp1
             Console.WriteLine("테스트");
 
             Console.WriteLine("분기1 변경1");
-
+            Console.WriteLine("새로반영");
             /***** 새로운 분기 #0001 ********/
             Console.WriteLine("#0001 분기 변경1");
 
